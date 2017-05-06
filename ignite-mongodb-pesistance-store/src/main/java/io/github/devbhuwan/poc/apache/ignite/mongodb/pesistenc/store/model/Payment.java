641ce8098d37f7538a66036c341fe349328b3159
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-05-06 13:50:31 +0400

    add spring boot in ignite-mongodb-persistence-store

---
+++ ignite-mongodb-pesistance-store/src/main/java/io/github/devbhuwan/poc/apache/ignite/mongodb/pesistenc/store/model/Payment.java
@@ -0,0 +1,11 @@
+package io.github.devbhuwan.poc.apache.ignite.mongodb.pesistenc.store.model;
+
+/**
+ * @author Bhuwan Upadhyay
+ * @date 2017/05/06
+ */
+public class Payment {
+
+    private Long batchId;
+
+}
